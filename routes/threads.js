18186386d24da7089269323fc3e9535524bc30c6
cooperdozier <cooper.dozier@gmail.com>
2019-11-25 09:24:00 -0500

    show threads in one forum function in threads.js

--- routes/threads.js
+++ routes/threads.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Thread = require('../models').Thread;
 const MessageItem = require('../models').MessageItem;
-
+const Forum = require('../models').Forum;
 //Create a Thread
 router.post('/', function (req, res) {
     Thread.create(req.body)
@@ -24,6 +24,22 @@ router.get('/', (req, res) => {
 //             res.json({ threads })
 //         })
 // })
+// /api/threads/anime
+
+router.get('/:forum_name', (req, res) => {
+    Forum.findOne({ where: { forum_name: req.params.forum_name } })
+    .then(result => {
+        Thread.findAll({
+            where: {forumId: result.id} 
+            //  where: forum_name = "Anime",
+            // attributes: ['username','subject','updatedAt']
+        })
+            .then(threads => {
+                res.json({ threads })
+            })
+    })
+
+})
 
 router.get('/:id', (req, res) => {
     Thread.findByPk(req.params.id, {
